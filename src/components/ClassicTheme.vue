6dfd1b033d5dab25404ed327f73c6ef742006e1d
CallmeSetia <dwi.setia.fardhana@gmail.com>
2024-04-20 10:32:20 +0700

    Classic Theme - Update - Belum Clean

--- src/components/ClassicTheme.vue
+++ src/components/ClassicTheme.vue
@@ -194,7 +194,7 @@ async function setDataDeviceName(val: any) {
     </main>
     <footer class="py-6 md:px-8 md:py-0">
       <div class="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <div class="text-center text-sm leading-loose text-muted-foreground md:text-left"><span class="inline-block">
+        <div class="text-center text-xs leading-loose text-muted-foreground md:text-left"><span class="inline-block">
           Built and designed by <a
             href="#" target=""
             class="underline underline-offset-4 font-bold decoration-foreground"> workalogi </a></span>
